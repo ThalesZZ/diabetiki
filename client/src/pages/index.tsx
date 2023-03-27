a2dd528f55d12d08c6e11a5060b6c14c4a57bca1
ThalesZZ <thazars@hotmail.com>
2023-03-27 11:05:57 -0300

    starting themes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,26 @@
-import Head from 'next/head'
+import { OceanTheme } from '@/themes/ocean'
+import React from 'react'
+import styled, { css, ThemeContext, ThemeProvider } from 'styled-components'
+
+// DASHBOARD EXAMPLE: https://xd.adobe.com/ideas/wp-content/uploads/2020/10/7-Tips-for-Dashboard-UX-UI-3.jpg
 
 export default function Home() {
+  const [theme, setTheme] = React.useState(OceanTheme)
+
   return (
-    <>
-      <Head>
-        <title>diabetiki</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
-        
-      </main>
-    </>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <ThemeProvider theme={theme}>
+        <Container>
+        </Container>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   )
 }
+
+const Container = styled.main`
+  ${({ theme }) => css`
+  width: 100vw;
+  height: 100vh;
+      background-color: ${theme.bgColor};
+  `}
+`
---
+++ client/src/themes/ocean.ts
@@ -0,0 +1,7 @@
+import { Theme } from "./Theme";
+
+// https://i.pinimg.com/736x/4f/4f/a8/4f4fa88c1737705b2f45c8b7015a93ba.jpg
+
+export const OceanTheme: Theme = {
+    bgColor: '#172532'
+}
