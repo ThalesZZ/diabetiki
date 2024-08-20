f72fc6953e8c9ee7af363cee74f6c67172b1bb68
ThalesZZ <thazars@hotmail.com>
2024-08-20 20:42:03 -0300

    added eslint

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -2,7 +2,10 @@ import { PlusOutlined as NewIcon } from '@ant-design/icons'
 import { FloatButton } from 'antd'
 
 export default function Page() {
-    return <>Hello, Next.js!
+  return (
+    <>
+      Hello, Next.js!
       <FloatButton type="primary" icon={<NewIcon />} tooltip={'New record'} />
     </>
-  }
+  )
+}
