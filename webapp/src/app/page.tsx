bc140e363b337fe00fc9238112a6313dac95b5bd
ThalesZZ <thazars@hotmail.com>
2024-08-21 01:47:16 -0300

    added tailwind

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,9 +1,5 @@
 import CreateGlucoseRecordModal from './(create-modals)/create-glucose-record-modal'
 
 export default function Page() {
-  return (
-    <>
-      <CreateGlucoseRecordModal />
-    </>
-  )
+  return <CreateGlucoseRecordModal />
 }
