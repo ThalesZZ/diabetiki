9ce245d193ec879bb4a6628f57624a8393463f46
ThalesZZ <thazars@hotmail.com>
2024-08-21 01:34:52 -0300

    added eslint to backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,20 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors";
+import express from "express";
 
-import { registerGlucoseRoutes } from './routes/glucose'
+import { registerGlucoseRoutes } from "./routes/glucose";
 
-const app = express()
-const port = 3030
+const app = express();
+const port = 3030;
 
-app.use(cors({    origin: true}))
-app.use(express.json())
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Diabetiki server is alive!')
-})
+app.get("/", (req, res) => {
+  res.send("Diabetiki server is alive!");
+});
 
-registerGlucoseRoutes(app)
+registerGlucoseRoutes(app);
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}.`)
-})
+  console.log(`Listening to port ${port}.`);
+});
--- server/src/routes/glucose.ts
+++ server/src/routes/glucose.ts
@@ -1,16 +1,15 @@
 import { Express, Router } from "express";
 
 export async function registerGlucoseRoutes(app: Express) {
-    const router = Router()
+  const router = Router();
 
+  app.post("/glucose", async function (req, res) {
+    // const evt = await prisma.glucoseEvent.findFirst()
 
-    app.post('/glucose', async function(req, res) {
-        // const evt = await prisma.glucoseEvent.findFirst()
-        
-        // res.send(evt?.id || 'not founddd')
-        console.log(req.body)
-        res.send(req.body)
-    })
+    // res.send(evt?.id || 'not founddd')
+    console.log(req.body);
+    res.send(req.body);
+  });
 
-    app.use(router)
+  app.use(router);
 }
