161dece5a8e49b90e2c33ac7c489761f36d3f4d6
ThalesZZ <thazars@hotmail.com>
2024-08-22 19:54:27 -0300

    fixed float button bug

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AntdRegistry>
-          <Layout>
+          <Layout id="app">
             <Sider />
 
             <LayoutContent>{children}</LayoutContent>
